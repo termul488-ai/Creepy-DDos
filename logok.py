23f8a905403edd0b54a06c663e4eb4c196e02f5c
termul488-ai <termul488@gmail.com>
2026-01-08 16:47:18 +0700

    Update logok.py

--- logok.py
+++ logok.py
@@ -149,7 +149,7 @@ def check_input(config):
 def main():
     threads_pool = []                        # Stores all active threads
     Header()                                 # Prints the header
-         options = get_parser()                   # Get the arguments
+         options = get_parser()              # Get the arguments
          config = check_input(vars(options.parse_args()))
 
     # PRINT INFO BANNER
