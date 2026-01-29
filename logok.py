20098988a5bc98bf338d4714cde28444da49bbd4
termul488-ai <termul488@gmail.com>
2026-01-29 23:18:50 +0700

    Update logok.py

--- logok.py
+++ logok.py
@@ -69,6 +69,7 @@ def attack():
       for y in range(100):
           print()
           atk.send(str.encode(request))
+          print("http://" + str(ip)
     except socket.error:
       time.sleep(.1)
     except:
